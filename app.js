952ebb191366c66f81a8bbf68331d8106a5bcb7c
Emilio <emiliothomasking@gmail.com>
2022-07-13 12:56:32 -0500

    added generate page funcionality

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template')
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-  
-    console.log('================');
-  
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+const [name, github] = profileDataArgs;
 
-};
-  
-  printProfileData(profileDataArgs);
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
